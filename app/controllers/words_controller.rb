6599d65934ff75d1b686edc866d6015cff7d2bd4
Shogo Hida <shogo.hida@gmail.com>
2020-06-07 00:53:05 +0900

    created basic controllers

---
+++ app/controllers/words_controller.rb
@@ -0,0 +1,33 @@
+class WordsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+
+  end
+
+  def edit
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def word_params
+
+  end
+end
+
+
